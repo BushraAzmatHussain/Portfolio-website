f5747b074b6745da007de7edad778d63ae2ead32
BushraAzmatHussain <bushrahussain407@gmail.com>
2021-11-30 22:01:31 +0600

    updated

--- script.js
+++ script.js
@@ -38,3 +38,42 @@ var form = document.getElementById("my-form");
       });
     }
     form.addEventListener("submit", handleSubmit)
+
+
+    //
+    
+    // typing text animation script
+    var typed = new Typed(".typing", {
+      strings: ["Frontend Developer ", "MERN Stack Developer"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+  });
+  var typed = new Typed(".typing-2", {
+      strings: ["Frontend Developer ", "MERN Stack Developer"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+  });
+  // owl carousel script
+  $('.carousel').owlCarousel({
+      margin: 20,
+      loop: true,
+      autoplay: true,
+      autoplayTimeOut: 2000,
+      autoplayHoverPause: true,
+      responsive: {
+          0:{
+              items: 1,
+              nav: false
+          },
+          600:{
+              items: 2,
+              nav: false
+          },
+          1000:{
+              items: 3,
+              nav: false
+          }
+      }
+  });
